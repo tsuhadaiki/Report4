2ab4d293e4084f6591b4203d59431d18936df021
Daiki Tsuha <e175730@ie.u-ryukyu.ac.jp>
2017-11-19 23:05:44 +0900

    first commit

---
+++ src/main/jp/ac/uryukyu/ie/e175730/Exhandling.java
@@ -0,0 +1,8 @@
+package jp.ac.uryukyu.ie.e175730;
+
+public class Exhandling {
+    public static void main(String[] args){
+        String str =  null;
+        System.out.println(str.length());
+    }
+}
