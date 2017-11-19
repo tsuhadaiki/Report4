a97ee1cc1006adb463736fb39a89527d6ec9b95f
Daiki Tsuha <e175730@ie.u-ryukyu.ac.jp>
2017-11-19 23:52:18 +0900

    try-catch構文を追加

--- src/main/jp/ac/uryukyu/ie/e175730/Exhandling.java
+++ src/main/jp/ac/uryukyu/ie/e175730/Exhandling.java
@@ -2,7 +2,12 @@ package jp.ac.uryukyu.ie.e175730;
 
 public class Exhandling {
     public static void main(String[] args){
-        String str =  null;
-        System.out.println(str.length());
+        try {
+            String str = null;
+            System.out.println(str.length());
+        }catch (NullPointerException e){
+            System.out.println("NullPointerExceptionが発生している");
+            System.out.println("エラー"+e.getMessage());
+        }
     }
 }
